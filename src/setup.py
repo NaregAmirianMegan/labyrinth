d96594a7cdeab02026a30ee0f9f7aa191f8c7819
Nareg Megan <nareg@calvisitor-10-105-166-208.calvisitor.1918.berkeley.edu>
2019-05-10 22:14:12 -0700

    build file

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,10 @@
 #External imports
 import cx_Freeze
 
-executables = [cx_Freeze.Excecutable("Main.py")]
+executables = [cx_Freeze.Executable("Main.py")]
 
 cx_Freeze.setup(
 				name="Labyrinth",
-				options={"build_exe": {"packages": ["pygame"], "included_files": ["../assets/images/L1.png"]}},
+				options={"build_exe": {"packages": ["pygame"], "include_files": ["../assets/images/L1.png"]}},
 				executables=executables
 				)
